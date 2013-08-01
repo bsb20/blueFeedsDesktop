fe405d91d5d08b8ae49fd826a36ae65d9c880563
benberg <ben.berg@duke.edu>
2013-08-01 17:31:47 -0400

    news reporting

---
+++ articleSubmit.php
@@ -0,0 +1,42 @@
+<?php
+	/* Updates the xml file within /desktop/bluefeedsTest.xml */
+	/* Takes the following values: title, link, date, description */
+	session_start();
+	$filepath = "./blueFeedsTest.xml";
+
+	if (file_exists($filepath)) {
+ 		$title=$_POST["title"];
+		$link=$_POST["link"];
+		$date=$_POST["date"];		
+		$desc=$_POST["description"]; 	
+
+		$rss = file_get_contents($filepath);
+		$dom = new DOMDocument();	
+		$dom->loadXML($rss);
+
+		$channelList = $dom->getElementsByTagName('channel');
+		$channel = $channelList->item(0);		
+		$item = $dom->createElement('item');
+		$item->appendChild($dom->createElement('title', $title));		
+		$item->appendChild($dom->createElement('link', $link));		
+		$item->appendChild($dom->createElement('date', $date));	
+		$item->appendChild($dom->createElement('description', $desc));		
+		$channel->appendChild($item);
+
+		$rss = $dom->saveXML();	
+		$bytes = file_put_contents($filepath, $rss);
+		if($bytes)
+		{
+                echo "true";
+		//	header('Location: http://bluefeeds.cs.duke.edu/home/htdocs/desktop/RSS Feeds.php?success=' . $bytes);			
+		}
+		else
+		{
+		//	header('Location: http://bluefeeds.cs.duke.edu/home/htdocs/desktop/RSS Feeds.php?failure=' . $bytes);				
+		}
+	}
+	else
+	{
+		echo "File missing";
+	}
+?>
